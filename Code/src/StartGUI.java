1f331115a4eda3c3ea37fa2a3ba171d082324ed8
bkelly0729 <bkelly0729@gmail.com>
2015-11-24 13:31:28 -0600

    11/24
    Added TestGUI and fixed StartGUI

--- Code/src/StartGUI.java
+++ Code/src/StartGUI.java
@@ -1,9 +1,14 @@
+import java.awt.BorderLayout;
 import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
 
-public class StartGUI {
+public class StartGUI extends JFrame {
+	StartGUI() {
     JFrame frame = new JFrame();
     frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     
@@ -13,7 +18,8 @@ public class StartGUI {
        {
           public void actionPerformed(ActionEvent event)
           {
-             JOptionPane.showMessageDialog(null,"shit")
+             JOptionPane.showMessageDialog(null,"shit");
           }
        });
+	}
 }
---
+++ Code/src/TestGUI.java
@@ -0,0 +1,10 @@
+
+public class TestGUI {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		  StartGUI myStartGUI = new StartGUI();
+	}
+
+}
