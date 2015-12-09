4840097e29a89b5243df2f62e95b8f7788183854
t33soccer <t33soccer@gmail.com>
2015-12-09 10:16:20 -0600

    testChange

--- Code/src/Redbox.java
+++ Code/src/Redbox.java
@@ -1702,6 +1702,9 @@ public class Redbox {
 		ViewRentablesGUI.add(textField_20);
 		textField_20.setColumns(10);
 		
+		//different commment
+		
+		
 		JButton btnBack_6 = new JButton("Back");
 		btnBack_6.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
